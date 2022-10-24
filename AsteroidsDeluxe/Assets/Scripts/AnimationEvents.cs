1309be048016aa48ac10af644667d4e615ac9066
EdRogers4 <59186908+EdRogers4@users.noreply.github.com>
2022-10-23 20:56:12 -0400

    Imported sounds

--- AsteroidsDeluxe/Assets/Scripts/AnimationEvents.cs
+++ AsteroidsDeluxe/Assets/Scripts/AnimationEvents.cs
@@ -6,6 +6,9 @@ public class AnimationEvents : MonoBehaviour
 {
     [SerializeField] private GameManager _scriptGameManager;
     [SerializeField] private Player _scriptPlayer;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioGameStart;
+    [SerializeField] private AudioClip _audioGameOver;
 
     public void PlayerReady()
     {
@@ -13,6 +16,12 @@ public class AnimationEvents : MonoBehaviour
         _scriptGameManager.NextLevel();
         _scriptGameManager.SetLives(2);
         _scriptPlayer.PlayerStart();
+        _audioSource.PlayOneShot(_audioGameStart, 0.5f);
+    }
+
+    public void GameOver()
+    {
+        _audioSource.PlayOneShot(_audioGameOver, 1.5f);
     }
 
     public void ResetStartButton()
